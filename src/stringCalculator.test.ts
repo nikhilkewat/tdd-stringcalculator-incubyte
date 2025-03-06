cae531cb2de82ae1b3a948f0e1b064abe5615510
NIkhil Kewat <kewatnikhil@gmail.com>
2025-03-06 14:17:16 +0530

    Test 4, 5
    - Sum of two numbers
    - Allow new line character instead of ","

--- src/stringCalculator.test.ts
+++ src/stringCalculator.test.ts
@@ -1,20 +1,36 @@
-import {add} from "./stringCalculator";
+import { add } from "./stringCalculator";
 
 describe("String Calculator", () => {
     it("should return Application Started", () => {
     });
 
+    //Test-1
     it(`should return 0 for empty string`, () => {
         expect(add("")).toBe(0);
     });
 
+    //Test-2
     it(`should return 0 for empty string whith white spaces`, () => {
         expect(add("     ")).toBe(0);
     });
 
+    //Test-3
     it("should return the number itself if there is only one number", () => {
         expect(add("1")).toBe(1);
     });
 
+    //Test-4
+    it("should return the sum of two numbers", () => {
+        expect(add("1,3")).toBe(4);
+        expect(add("4,5")).toBe(9);
+    });
+
+    //Test-5
+    it(`should allow new lines "\\n" between numbers instead of ","`, () => {
+        expect(add("1\n3")).toBe(4);
+        expect(add("4 \n 5")).toBe(9);
+    });
 });
+
+
 
--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -1,11 +1,25 @@
 export const add = (numbers: string): number => {
 
-    if(!numbers) return 0;
-    
-    const _numbers = numbers.split(",").map((num) => parseInt(num.trim(), 10)).filter((num) => !isNaN(num));
+    if (!numbers) return 0;
+
+    const _numbers = extractValues(numbers);
 
     if (_numbers.length === 0) return 0;
     else if (_numbers.length === 1) return _numbers[0];
-    else return NaN;
+    else return _numbers.reduce((acc, num) => acc + num, 0);
 }
 
+
+const extractValues = (value: string): number[] => {
+
+    let delimiter = [",", "\n"];
+    let numberString = value;
+
+    const regex = new RegExp(`[${delimiter.join("")}]`);
+    const numbers = numberString
+        .split(regex)
+        .map((num) => parseInt(num.trim(), 10))
+        .filter((num) => !isNaN(num));
+
+    return numbers;
+}
