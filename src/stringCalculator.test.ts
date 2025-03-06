0db3733ad79e7058fc62e3d5fce501b8bf19c32c
NIkhil Kewat <kewatnikhil@gmail.com>
2025-03-06 13:51:59 +0530

    Test 2 - return number itself
    Return Number itself if there is one number

--- src/stringCalculator.test.ts
+++ src/stringCalculator.test.ts
@@ -12,5 +12,9 @@ describe("String Calculator", () => {
         expect(add("     ")).toBe(0);
     });
 
+    it("should return the number itself if there is only one number", () => {
+        expect(add("1")).toBe(1);
+    });
+
 });
 
--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -1,3 +1,11 @@
 export const add = (numbers: string): number => {
-    return numbers.trim() === "" ? 0 : parseInt(numbers);
+
+    if(!numbers) return 0;
+    
+    const _numbers = numbers.split(",").map((num) => parseInt(num.trim(), 10)).filter((num) => !isNaN(num));
+
+    if (_numbers.length === 0) return 0;
+    else if (_numbers.length === 1) return _numbers[0];
+    else return NaN;
 }
+
